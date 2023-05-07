c9cf7456dd1590c1337f3d4345f9236e1e99b0e0
xCondorellix <128437636+xCondorellix@users.noreply.github.com>
2023-05-06 23:06:54 -0300

    Delete Coderhouse Projecto Final/WebBlogAuto directory

--- Coderhouse Projecto Final/WebBlogAuto/Core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Auto(models.Model):
-    name = models.CharField(max_length= 50)
-    brand = models.CharField(max_length= 30)
-    year = models.CharField(max_length= 4)
-    price = models.IntegerField()
